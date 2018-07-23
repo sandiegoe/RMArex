af6ce7898923030f579fe8ce55bd83725d304eb3
arexstorm <arexstorm@arexstorm.top>
2018-07-23 13:59:19 +0800

    搜索页数据展现和下拉刷新,上拉加载更多, movies页面添加下拉刷新

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -8,20 +8,28 @@ Page({
    */
   data: {
     containerPanelShow: true,
-    searchPanelShow: false
+    searchPanelShow: false,
+    searchUrl: "",
+    searchMovies: {
+      movies: []
+    },
+    totalCount: 0,
+    inTheatersUrl: app.globalData.g_doubanBase + "/v2/movie/in_theaters?start=0&count=3",
+    comingSoonUrl: app.globalData.g_doubanBase + "/v2/movie/coming_soon?start=0&count=3",
+    top250Url: app.globalData.g_doubanBase + "/v2/movie/top250?start=0&count=3"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var inTheatersUrl = app.globalData.g_doubanBase + "/v2/movie/in_theaters?start=0&count=3";
-    var comingSoonUrl = app.globalData.g_doubanBase + "/v2/movie/coming_soon?start=0&count=3";
-    var top250Url = app.globalData.g_doubanBase + "/v2/movie/top250?start=0&count=3";
 
-    this.getRequestData(inTheatersUrl, 'inTheaters', '正在热映');
-    this.getRequestData(comingSoonUrl, 'comingSoon', '即将上映');
-    this.getRequestData(top250Url, 'top250', 'Top250');
+    wx.showNavigationBarLoading();
+    this.getRequestData(this.data.inTheatersUrl, 'inTheaters', '正在热映');
+    wx.showNavigationBarLoading();
+    this.getRequestData(this.data.comingSoonUrl, 'comingSoon', '即将上映');
+    wx.showNavigationBarLoading();
+    this.getRequestData(this.data.top250Url, 'top250', 'Top250');
   },
 
   onBindFocus: function(event) {
@@ -36,17 +44,27 @@ Page({
     console.info('onCalcelSearchTab...');
     this.setData({
       containerPanelShow: true,
-      searchPanelShow: false
+      searchPanelShow: false,
+      searchMovies: {
+        movies: []
+      }
     });
   },
 
   onBindChange: function(event) {
     console.info('onBindChange');
-    this.setData({
-      containerPanelShow: true,
-      searchPanelShow: false,
-      searchContent: ''
-    });
+    console.info(event);
+    var search = event.detail.value;
+    if (!search) {
+      console.info("balank");
+      return;
+    }
+    console.info(search);
+    var searchUrl = app.globalData.g_doubanBase + "/v2/movie/search?q=" + search;
+    wx.showNavigationBarLoading();
+    this.getRequestData(searchUrl, "searchMovies", null);
+    this.data.searchUrl = searchUrl;
+    this.data.totalCount = this.data.totalCount + 20;
   },
 
   moreMovies: function(event) {
@@ -66,13 +84,22 @@ Page({
       },
       success: function (res) {
         console.info(res);
-        var movies = util.processDoubanData(res.data);
         var data = {};
+        var movies = util.processDoubanData(res.data);
+        if (selectedKey == 'searchMovies') { // searchMovies 的话 追加
+          console.info("原有：" + that.data[selectedKey]);
+          console.info("原有的movies：" + that.data[selectedKey].movies);
+          movies = that.data[selectedKey].movies.concat(movies);
+        }
         data[selectedKey] = {
           movies: movies,
           categoryName: categoryName
         };
         that.setData(data);
+        wx.hideNavigationBarLoading();
+        // if (selectedKey == 'searchMovies') {
+          wx.stopPullDownRefresh();
+        // }
       }
     })
   },
@@ -109,14 +136,39 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    if (this.data.searchPanelShow) { //搜索页
+      var searchUrl = this.data.searchUrl;
+      searchUrl = searchUrl + "&start=0&count=" + 20;
+      this.data.searchMovies = { movies: [] }; //清空原有的movies
+      wx.showNavigationBarLoading();
+      this.getRequestData(searchUrl, "searchMovies", null);
+      this.data.totalCount = 20;
+    } else {
+      console.info("no search page pull down refersh...");
+      wx.showNavigationBarLoading();
+      this.getRequestData(this.data.inTheatersUrl, 'inTheaters', '正在热映');
+      wx.showNavigationBarLoading();
+      this.getRequestData(this.data.comingSoonUrl, 'comingSoon', '即将上映');
+      wx.showNavigationBarLoading();
+      this.getRequestData(this.data.top250Url, 'top250', 'Top250');
+      wx.stopPullDownRefresh();
+    }
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    if (this.data.searchPanelShow) { //搜索页
+      var searchUrl = this.data.searchUrl;
+      var totalCount = this.data.totalCount;
+      searchUrl = searchUrl + "&start=" + totalCount + "&count=" + 20;
+      wx.showNavigationBarLoading();
+      this.getRequestData(searchUrl, "searchMovies", null);
+      this.data.totalCount = totalCount + 20;
+    } else {
+      console.info("no search page reach bottom....");
+    }
   },
 
   /**
